db3177ca8b981bfe1eb37d59bdece2a4639a2091
enjuichang <55577469+enjuichang@users.noreply.github.com>
2021-08-25 14:59:20 +0800

    resolve SSL error

--- response_parser.py
+++ response_parser.py
@@ -4,22 +4,11 @@
 import covid_info_bot
 import requests
 
-import certifi
-# import urllib3
+vaccine_stock_url = 'https://covid-19.nchc.org.tw/api/covid19?CK=covid-19@nchc.org.tw&querydata=2001'
 
-# http = urllib3.PoolManager(
-#     cert_reqs="CERT_REQUIRED",
-#     ca_certs=certifi.where()
-# )
+vaccine_stock = requests.get(url = vaccine_stock_url, verify=False)
 
-# vaccine_stock_url = 'https://covid-19.nchc.org.tw/api/covid19?CK=covid-19@nchc.org.tw&querydata=2001'
-test_url = "https://covid-19.nchc.org.tw/api/covid19?CK=covid-19@nchc.org.tw&querydata=5001&limited=全部縣市"
-
-vaccine_stock_url = "https://covid-19.nchc.org.tw/api/covid19"
-#vaccine_stock_api = requests.get(url = vaccine_stock_url, verify=certifi.where())
-vaccine_stock_api = requests.post(url=vaccine_stock_url, data = {"CK":"covid-19@nchc.org.tw", "querydata":"5001", "limited":"全部縣市"})
-
-print(vaccine_stock_api)
+print(vaccine_stock.content)
 
 def parse_response(inputSTR):
     response = covid_info_bot.runLoki(inputSTR)
