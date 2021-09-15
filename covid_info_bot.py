805a00a1fb1b5399d45cf575a6a9db88bfef2a6c
ShiShanLiu <a37079962@gmail.com>
2021-09-15 16:14:48 +0800

    update covid_info_bot

--- covid_info_bot.py
+++ covid_info_bot.py
@@ -230,7 +230,7 @@ if __name__ == "__main__":
     # print("")
 
     # 輸入其它句子試看看
-    inputLIST = ["az嚴重副作用"]
+    inputLIST = ["az副作用"]
     filterLIST = []
     resultDICT = runLoki(inputLIST, filterLIST)
     print(resultDICT)
